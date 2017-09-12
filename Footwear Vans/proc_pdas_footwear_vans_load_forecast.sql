e378e3f5caa159fb2a1e3fdaa1cd5a0cc579cb22
chaos87 <jonathan.barone@ebp-global.com>
2017-09-12 15:35:46 +0800

    added product size master

---
+++ Footwear Vans/proc_pdas_footwear_vans_load_forecast.sql
@@ -0,0 +1,11 @@
+-- ==============================================================
+-- Author:		ebp Global
+-- Create date: 9/12/2017
+-- Description:	Procedure to load the forecast in fact_forecast.
+-- ==============================================================
+ALTER PROCEDURE [dbo].[mc_table_mc_dashboard_item]
+	@output_param nvarchar(500) OUTPUT
+AS
+BEGIN
+
+END
