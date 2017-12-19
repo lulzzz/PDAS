8b140b8a3a900b3fb108a1a51bd5499747bf0d02
ebpFlorianGamper <florian.gamper@ebp-solutions.com>
2017-12-19 08:39:40 +0800

    test on step01

--- Console/Steps/mc_step_pdas_footwear_vans_01.sql
+++ Console/Steps/mc_step_pdas_footwear_vans_01.sql
@@ -10,57 +10,16 @@ GO
 -- Description:	Console procedure to run the PDAS Footwear Vans step 01.
 -- =============================================
 ALTER PROCEDURE [dbo].[mc_step_pdas_footwear_vans_01]
-	@mc_user_name nvarchar(100) = NULL
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	DECLARE @mc_system_name nvarchar(100) = 'pdas_ftw_vans'
-	DECLARE @mc_proc_name nvarchar(100) = 'mc_step_pdas_footwear_vans_01'
+	WAITFOR DELAY '00:00:05'
 
-	IF (@mc_user_name IN (SELECT [name] FROM [dbo].[mc_user]))
-	BEGIN
+	-- Start new PDAS release
+	-- EXEC [proc_pdas_footwear_vans_create_system_key] @mc_user_name = @mc_user_name
 
-        -- Initialize Console procedure table (this is the 1st step for Footwear and Apparel)
-        UPDATE [dbo].[mc_proc]
-        SET
-            [status_start] = 0
-            ,[status_end] = 0
-        WHERE
-            [mc_system_name] = @mc_system_name
-
-        -- Update Console procedure table
-        UPDATE [dbo].[mc_proc]
-        SET
-            [update_dt] = GETDATE()
-            ,[status_start] = 1
-        WHERE
-            [mc_system_name] = @mc_system_name
-            AND [name] = @mc_proc_name
-
-        -- Stored procedure(s) to run
-        /* START */
-        -- Step 01 - Start new PDAS release
-        EXEC [proc_pdas_footwear_vans_create_system_key] @mc_user_name = @mc_user_name
-		-- Copy previous release as current release
-		EXEC [proc_pdas_footwear_vans_create_copy_previous_release]
-        /* END */
-
-        -- Update Console procedure table
-		UPDATE [dbo].[mc_proc]
-		SET
-			[update_dt] = GETDATE()
-			,[status_end] = 1
-		WHERE
-			[mc_system_name] = @mc_system_name
-			AND [name] = @mc_proc_name
-
-	END
-	ELSE
-	BEGIN
-
-		RETURN -500
-
-	END
+	-- Copy previous release as current release
+	-- EXEC [proc_pdas_footwear_vans_create_copy_previous_release]
 
 END
