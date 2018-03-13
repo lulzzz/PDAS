ae58d81f8dbef2ffcdc7ce2b9a9cfe04f2a71d1b
ebpFlorianGamper <florian.gamper@ebp-solutions.com>
2018-03-13 09:30:50 +0800

    test file

--- Tests/check_ngc_imports.sql
+++ Tests/check_ngc_imports.sql
@@ -1,20 +1,33 @@
 SELECT
-	year(original_crd_dt) y,
-	month(original_crd_dt) m,
-	sum(order_qty) qty,
-	count(*) c
-  FROM
-	[dbo].[staging_pdas_footwear_vans_ngc_po_init2017]
-group by year(original_crd_dt), month(original_crd_dt)
-order by y, m
-
-
-SELECT
 	year(convert(date, [actual_crd_dt])) y,
 	month(convert(date, [actual_crd_dt])) m,
 	sum(convert(int, order_qty)) qty,
 	count(*) c
   FROM
-	[dbo].[staging_pdas_footwear_vans_ngc_po_csv_text]
+	[dbo].[staging_pdas_footwear_vans_ngc_po]
 group by year(convert(date, [actual_crd_dt])), month(convert(date, [actual_crd_dt]))
 order by y, m
+
+
+select
+    year(convert(date, dd.full_date)) y,
+	month(convert(date, dd.full_date)) m,
+	sum(quantity)
+from
+  fact_demand_total as f_1
+  INNER JOIN dim_demand_category
+       ON f_1.dim_demand_category_id = dim_demand_category.id
+   INNER JOIN dim_date dd
+       ON f_1.dim_date_id = dd.id
+where
+   dim_pdas_id = (select max(id) from dim_pdas)
+   and dim_demand_category.name in (
+       'Open Order',
+       'Shipped Order'
+   )
+group by
+    year(convert(date, dd.full_date)) y,
+	month(convert(date, dd.full_date)) m
+order by
+    year(convert(date, dd.full_date)) y,
+	month(convert(date, dd.full_date)) m
